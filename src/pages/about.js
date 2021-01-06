5a7207e5aa90ea68ca01bae61bbec91fd56c9263
Anna Kijania <kijania.anna@gmail.com>
2021-01-06 22:08:09 +0100

    Edit about

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,17 +9,10 @@ const About = ({ data: { about } }) => (
     <article className="sheet">
       <HelmetDatoCms seo={about.seoMetaTags} />
       <div className="sheet__inner">
-        <h1 className="sheet__title">{about.title}</h1>
-        <p className="sheet__lead">{about.subtitle}</p>
         <div className="sheet__gallery">
           <Img fluid={about.photo.fluid} />
         </div>
-        <div
-          className="sheet__body"
-          dangerouslySetInnerHTML={{
-            __html: about.bioNode.childMarkdownRemark.html,
-          }}
-        />
+
       </div>
     </article>
   </Layout>
